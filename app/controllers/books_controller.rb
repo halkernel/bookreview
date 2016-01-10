09d06fa2b5c6c1dff9baf05a5a4a8060f2d259dc
Kennet Emerson <kennet.emerson@gmail.com>
2016-01-09 22:45:55 -0300

    It was added a back, edit and delete button for the show view.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,6 @@ class BooksController < ApplicationController
 	end
 
 	def show
-		@book = Book.find(params[:id])
-
 	end
 
 	def new
@@ -22,6 +20,22 @@ class BooksController < ApplicationController
 		else
 			render 'new'
 		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @book.update(book_params)
+			redirect_to book_path(@book)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@book.destroy
+		redirect_to root_path
 	end
 
 
